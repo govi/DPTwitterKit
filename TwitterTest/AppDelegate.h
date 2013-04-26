2df5f5e3613227affe4229c6e0fff8d914278694
Govi <govi@email.com>
2013-04-26 15:53:14 +0100

    Initial Commit

---
+++ TwitterTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TwitterTest
+//
+//  Created by Govi on 26/04/2013.
+//  Copyright (c) 2013 Genie-Connect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TwitterTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TwitterTest
+//
+//  Created by Govi on 26/04/2013.
+//  Copyright (c) 2013 Genie-Connect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
